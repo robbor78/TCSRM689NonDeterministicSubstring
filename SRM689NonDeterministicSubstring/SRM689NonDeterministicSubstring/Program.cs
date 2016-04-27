47edcf784f9a55a41cfd3b43e7d88c844eb496d8
name <email@email.com>
2016-04-27 13:58:01 +0200

    init

---
+++ SRM689NonDeterministicSubstring/SRM689NonDeterministicSubstring/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SRM689NonDeterministicSubstring
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      String a = "00010001";
+      String b = "(0|1)(0|1)";
+
+      Regex rexp = new Regex(b, RegexOptions.None, Regex.InfiniteMatchTimeout);
+
+      var matches = rexp.Matches(a);
+      Console.Out.WriteLine("Matches...");
+      foreach (var m in matches)
+      {
+        Console.Out.WriteLine(m);
+      }
+
+      var splits = rexp.Split(a);
+      Console.Out.WriteLine("Splits...");
+      foreach (var s in splits)
+      {
+        Console.Out.WriteLine(s);
+      }
+
+    }
+
+    public long ways(String A, String B)
+    {
+      return -1;
+    }
+  }
+}
---
+++ SRM689NonDeterministicSubstring/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SRM689NonDeterministicSubstring;
+
+namespace UnitTestProject1
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+      string[] aS = { "00010001" };
+      string[] bS = { "??" };
+      int[] ways = { 3 };
+
+      int length = aS.Length;
+
+      for (int i =0; i<length; i++)
+      {
+        string a = aS[i];
+        string b = bS[i];
+
+        Program target = new Program();
+        long actual = target.ways(a, b);
+
+        long expected = ways[i];
+        Assert.AreEqual(actual, expected);
+      }
+    }
+  }
+}
