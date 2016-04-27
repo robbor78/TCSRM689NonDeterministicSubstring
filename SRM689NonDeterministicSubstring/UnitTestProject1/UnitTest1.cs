b78345cc397362b9b7e8ddf1db9390244c53fc50
name <email@email.com>
2016-04-27 15:38:58 +0200

    more tests

--- SRM689NonDeterministicSubstring/UnitTestProject1/UnitTest1.cs
+++ SRM689NonDeterministicSubstring/UnitTestProject1/UnitTest1.cs
@@ -10,13 +10,13 @@ namespace UnitTestProject1
     [TestMethod]
     public void TestMethod1()
     {
-      string[] aS = { "00010001" };
-      string[] bS = { "??" };
-      int[] ways = { 3 };
+      string[] aS = { "00010001", "00000000", "001010101100010111010", "00101", "1101010101111010101011111111110001010101" };
+      string[] bS = { "??", "??0??0", "???", "??????", "???????????????????????????????????" };
+      int[] ways = { 3, 1, 8, 0, 6 };
 
       int length = aS.Length;
 
-      for (int i =0; i<length; i++)
+      for (int i = 0; i < length; i++)
       {
         string a = aS[i];
         string b = bS[i];
